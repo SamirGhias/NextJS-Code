6c6df30c98ec99bb631f0de089327b89d3e57afd
SamirGhias <samir.ghias99@gmail.com>
2024-05-21 15:28:06 -0400

    complete upto vid 123, with error

--- Foodies Project/app/meals/share/page.js
+++ Foodies Project/app/meals/share/page.js
@@ -2,6 +2,7 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
   // shareMeal();
@@ -45,7 +46,8 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label='Your image' name='image' />
           <p className={classes.actions}>
-            <button type='submit'>Share Meal</button>
+            {/* <button type='submit'>Share Meal</button> */}
+            <MealsFormSubmit></MealsFormSubmit>
           </p>
         </form>
       </main>
