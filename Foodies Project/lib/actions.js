29a12d535b76bab755d6bda8cb7217684eb3d46f
Samir Ghias <70853406+SamirGhias@users.noreply.github.com>
2024-06-05 15:01:19 -0400

    finished project

--- Foodies Project/lib/actions.js
+++ Foodies Project/lib/actions.js
@@ -1,18 +1,20 @@
-'use server';
+"use server";
 
-import { redirect } from 'next/navigation';
-import { saveMeal } from './meals';
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 export async function shareMeal(formData) {
-  'use server';
+  "use server";
   const meal = {
-    title: formData.get('title'),
-    summary: formData.get('summary'),
-    instructions: formData.get('instructions'),
-    image: formData.get('image'),
-    creator: formData.get('name'),
-    creator_email: formData.get('email'),
+    title: formData.get("title"),
+    summary: formData.get("summary"),
+    instructions: formData.get("instructions"),
+    image: formData.get("image"),
+    creator: formData.get("name"),
+    creator_email: formData.get("email"),
   };
   await saveMeal(meal);
-  redirect('/meals');
+  revalidatePath("/meals");
+  redirect("/meals");
 }
